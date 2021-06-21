d31a2f0ed6347e17fd35fc8f48499dd6c8336503
ParshvaJain <pashwab255225@gmail.com>
2021-06-21 11:54:28 +0530

    third commit

--- src/main/java/com/stockexchange/StockExchangeApplication.java
+++ src/main/java/com/stockexchange/StockExchangeApplication.java
@@ -2,8 +2,6 @@ package com.stockexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
 
 @SpringBootApplication
 public class StockExchangeApplication {

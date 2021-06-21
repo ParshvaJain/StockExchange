035414c00cc96c8ac0e9a5a0972a5430f2e851f3
ParshvaJain <pashwab255225@gmail.com>
2021-06-21 15:47:20 +0530

    cors enabled

--- src/main/java/com/stockexchange/api/StockExchangeController.java
+++ src/main/java/com/stockexchange/api/StockExchangeController.java
@@ -20,7 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-//@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins ="https://reactapp--service.herokuapp.com")
 @RestController
 @RequestMapping(value = "/stockexchange")
 public class StockExchangeController {
